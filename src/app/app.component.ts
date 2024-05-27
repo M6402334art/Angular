bb8111b172cff93fc4ef76252bab1eedd1d5ed62
rattaphong <rattaphong.u@gmail.com>
2024-05-27 14:59:23 +0700

    ep3.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'api';
-  ninjaName = 'Naruto';
+  // title = 'api';
+  // ninjaName = 'Naruto';
 
-  ninjaConsole() {
-    console.log('Naruto is the best ninja');
+  // ninjaConsole() {
+  //   console.log('Naruto is the best ninja');
+  // }
+
+  // changeNinjaName(name: string) {
+  //   this.ninjaName = name;
+  // }
+  value = 0
+  callBuffet(value: string) {
+    const price = +value
+    this.value = (price*3)/4
   }
-
-  changeNinjaName(name: string) {
-    this.ninjaName = name;
-  }
-
-
 }
